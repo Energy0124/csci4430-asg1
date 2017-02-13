1da7e126202945688bd190e0a06fe2de97922f9e
Energy0124 <energystar.0124@gmail.com>
2017-02-13 23:50:00 +0800

    reformat import

--- server.c
+++ server.c
@@ -1,11 +1,11 @@
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <string.h>
-# include <errno.h>
-# include <sys/socket.h>
-# include <sys/types.h>
-# include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 #include <pthread.h>
 #include <signal.h>
 
